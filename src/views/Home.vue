5f18ad9be7a54f5815212e4bfbd9dbb47e7bcd37
zhaozhiqiang <zhaozhiqiang@honghe-tech.com>
2019-11-18 17:43:36 +0800

    轮播放到banner里去

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -17,7 +17,11 @@
             </ul>
         </div>
         <div class="banner">
-
+            <el-carousel :interval="3000" type="card" height="200px">
+            <el-carousel-item v-for="item in 6" :key="item">
+              <h3 class="medium">{{ item }}</h3>
+            </el-carousel-item>
+          </el-carousel>
         </div>
         <div class="goods">
             <router-view></router-view>
@@ -44,7 +48,6 @@ export default {
     #main{
         width: 1200px;
         margin: 20px auto;
-        background-color: red;
     }
     .left{
         width: 150px;
@@ -78,7 +81,23 @@ export default {
     .banner{
         float: left;
         width: 1050px;
-        height: 200px;
-        background-color: green;
+    }
+    .goods{
+        float: left;
+        width: 1050px;
+        background-color: red;
+    }
+    .el-carousel__item h3 {
+        color: #475669;
+        font-size: 14px;
+        opacity: 0.75;
+        line-height: 200px;
+        margin: 0;
+    }
+    .el-carousel__item:nth-child(2n) {
+        background-color: #99a9bf;
+    }
+    .el-carousel__item:nth-child(2n+1) {
+        background-color: #d3dce6;
     }
 </style>
