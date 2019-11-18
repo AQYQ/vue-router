ee7191952b9bfd8fddaf0639d443fb0df805bf60
zhaozhiqiang <zhaozhiqiang@honghe-tech.com>
2019-11-18 14:47:57 +0800

    引入element-ui

--- src/components/Computer.vue
+++ src/components/Computer.vue
@@ -1,5 +1,7 @@
 <template>
     <div>
         我是電腦組件
+        <br>
+        <el-button type="primary">主要按钮</el-button>
     </div>
 </template>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -25,7 +25,6 @@
     </div>
 </template>
 <script>
-import $ from 'jquery'
 export default {
     created(){
         console.log('created------------');
